38dd0ec8e88d300cea972380de934fea402cdbd1
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-24 11:47:24 -0300

    fui até o exercicio de atribuição

---
+++ aula02/ex007.py
@@ -0,0 +1,5 @@
+# receber 3 valores e analisar o menor deles
+valor1 = 34;
+valor2 = 29;
+valor3 = 31;
+print(min(valor1, valor2, valor3))
---
+++ aula02/ex008.py
@@ -0,0 +1,5 @@
+#atribuir valores a variaveis e depois calcular elas com a formula a*a + b*b
+a = 3;
+b = 4;
+c = (a*a)+(b*b);
+print(c)
