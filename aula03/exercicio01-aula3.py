a257f790e8f23995a703508065e4ef189fd93933
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-25 16:44:59 -0300

    arrumei alguns detalhes

--- aula03/exercicio01-aula3.py
+++ aula03/exercicio01-aula3.py
@@ -1,7 +1,7 @@
 lado1 = int(input('Digite um valor para o lado 1: '))
 lado2 = int(input('Digite um valor para o lado 2: '))
 lado3 = int(input('Digite um valor para o lado 3: '))
-if (lado1 != 0 and lado2 != 0 and lado3 != 0):
+if (lado1 > 0 and lado2 > 0 and lado3 > 0):
     if (lado1 <= lado2 + lado3 and lado2 <= lado1 + lado3 and lado3 <= lado2 + lado1 ):
         if (lado1 == lado2 and lado2 == lado3 and lado1 == lado3):
             print('Triangulo Equilátero')
@@ -9,3 +9,7 @@ if (lado1 != 0 and lado2 != 0 and lado3 != 0):
             print('Triangulo Escaleno')
         else:
             print('Triangulo Isóceles')
+    else:
+        print('Um dos lados é maior que os outros somados.')
+else:
+    print('Valores invalidos!')
