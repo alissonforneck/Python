0a9f4f6b31cd21ddfb2909091032623588047726
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-25 16:38:01 -0300

    fiz o exercicio do triangulo

---
+++ aula03/exercicio01-aula3.py
@@ -0,0 +1,11 @@
+lado1 = int(input('Digite um valor para o lado 1: '))
+lado2 = int(input('Digite um valor para o lado 2: '))
+lado3 = int(input('Digite um valor para o lado 3: '))
+if (lado1 != 0 and lado2 != 0 and lado3 != 0):
+    if (lado1 <= lado2 + lado3 and lado2 <= lado1 + lado3 and lado3 <= lado2 + lado1 ):
+        if (lado1 == lado2 and lado2 == lado3 and lado1 == lado3):
+            print('Triangulo Equilátero')
+        elif (lado1 != lado2 and lado1 != lado3 and lado2 != lado3):
+            print('Triangulo Escaleno')
+        else:
+            print('Triangulo Isóceles')
