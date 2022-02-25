5b92cccc378907a1531270acffaedf83a220cdd2
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-25 14:40:42 -0300

    testando expresoes booleanas

---
+++ aula03/expressoes-booleanas.py
@@ -0,0 +1,20 @@
+teste = 2+2 < 4
+print(teste)
+
+teste = 7//3 == 1+1
+print(teste)
+
+teste = 3**2 +4**2 == 25
+print(teste)
+
+teste = 2+4+6 > 12
+print(teste)
+
+teste = 1387 % 19 == 0
+print(teste)
+
+teste = 31 % 2 == 0
+print(teste)
+
+teste = min(34,29,31) < 30
+print(teste)
