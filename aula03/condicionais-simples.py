121543cb2faf95a3fc430e0799e0fcf18270856d
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-25 14:57:04 -0300

    terminei os ex de condicionais simples

---
+++ aula03/condicionais-simples.py
@@ -0,0 +1,12 @@
+#idade = int (input('Digite sua idade: '))
+#if (idade > 60 ):
+#    print('Voce tem direito aos beneficios')
+
+#escudo = int(input('Qual sua quantidade de escudo'))
+#dano = int(input('Qual dano sofrido: '))
+#if (dano > 10 and escudo == 0):
+#   print('Voce esta morto')
+
+direcao = input('Digite sua direção: ')
+if (direcao == 'leste' or direcao =='norte' or direcao =='sul' or direcao =='oeste'):
+    print('Voce escapou')
