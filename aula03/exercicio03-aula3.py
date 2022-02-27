ce88a3589f84c18d47f292f7aed165a0595886b4
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-27 08:50:27 -0300

    arrumei alguns detalhes que poderiam melhorar

--- aula03/exercicio03-aula3.py
+++ aula03/exercicio03-aula3.py
@@ -1,22 +1,24 @@
-quantidade = int(input('Quantos kWh você consumiu? '))
+quantidade = float(input('Quantos kWh você consumiu? '))
 print('Existem 3 tipos de instalções de energia.')
 print('Tipo R : para instalações residenciais')
 print('Tipo I: para instalações indústriais')
 print('Tipo C: para instalaçoes comerciais')
 tipo = input('Qual o tipo de instalação você utiliza? ')
-if(tipo == 'R'):
+if(tipo == 'R' or tipo == 'r'):
     if (quantidade <= 500):
         preco = quantidade * 0.40
     elif(quantidade > 500):
         preco = quantidade * 0.65
-elif(tipo == 'C'):
+elif(tipo == 'C' or tipo == 'c'):
     if(quantidade <= 1000):
         preco = quantidade * 0.55
     elif(quantidade <= 5000):
         preco = quantidade * 0.60
-elif(tipo == 'I'):
+elif(tipo == 'I' or tipo == 'i'):
     if(quantidade <= 5000):
         preco = quantidade * 0.55
     else:
         preco = quantidade * 0.60
+else:
+    print('Instalação invalida')
 print('Voce utiliza a instalação do tipo {} e por isso precisa pagar {}'.format(tipo,preco))
