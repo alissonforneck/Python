5986c6aff234ba0e96bdce96bf0e624eb97ad447
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-28 22:15:54 -0300

    comecei os exercicios da aula 4

---
+++ aula04/estruturas-de-repeticao-01.py
@@ -0,0 +1,9 @@
+#realzie a sequencia de print com for e while:
+#inteiros de 3 ate 12 com o 12 inlcuso, e inteiros de 0 a 9 com o passo de 2
+
+contador = 3
+while contador <= 12:
+    print(contador)
+    contador += 1
+for i in range (0,10,2):
+    print(i)
