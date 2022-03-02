d1da3e31023d69a7c7b4740c8f45ffc6308ac3b7
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-03-02 19:00:23 -0300

    comecei a resolver os exercicios da aula 05

---
+++ aula05/docstrings.py
@@ -0,0 +1,14 @@
+def soma (x=0, y=0, z=0):
+    """
+    Retorna o somatório de at 3 valores numericos quaisquer.
+    Todos os parametros sao opcionais
+    x: valor numerico (opcional)
+    y: valor numerico (opcional)
+    z: valor numerico (opcional)
+    """
+    return x + y + z
+ 
+
+
+print(soma(3,2))
+help(soma)
