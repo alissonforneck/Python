684a61b2c180ae5678ba73f6ddd071f457e6da16
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-03-10 20:36:37 -0300

    começando a aula pratica 6

---
+++ aula06/funcoes-externas.py
@@ -0,0 +1,7 @@
+#import math forma correta
+#print(math.sqrt(9)
+
+#também podemos dar um apelido para a biblioteca, com as m no final
+#também podemos importar apenas a função desejada, seria da forma abaixo
+from math import  sqrt
+print(sqrt(9))
---
+++ aula06/praticando-listas.py
@@ -0,0 +1,13 @@
+lista = [9,7,7,10,3,9,6,6,2]
+print(lista.count(7))
+
+lista[-1] = 4
+print(lista)
+print(max(lista))
+print(lista.sort())
+tamanho = len(lista)
+media = 0
+for n in range(tamanho):
+    media += tamanho
+media /= tamanho
+print(media)
