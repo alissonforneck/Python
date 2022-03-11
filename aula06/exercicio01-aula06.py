baca8785b229b077cdfe9a87e268a910073b3c25
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-03-10 22:05:09 -0300

    terminei o primeiro exercico

---
+++ aula06/exercicio01-aula06.py
@@ -0,0 +1,7 @@
+palavras = ('primeira', 'segunda', 'terceira', 'quarta')
+
+for palavra in palavras:
+    print('\nPalavra: {}. Vogais: '.format(palavra.upper()), end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(letra.upper(),end=' ')
